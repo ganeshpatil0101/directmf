d829018666b82f39fef75d53d1b613484e5ae59b
Ganesh <shree@localhost.localdomain>
2020-02-05 23:32:14 +0530

    WIP Added mf required other fields and model

--- lib/models/mfData.dart
+++ lib/models/mfData.dart
@@ -3,26 +3,33 @@ import 'package:meta/meta.dart';
 import 'package:uuid/uuid.dart';
 
 class MFData {
-  // final String mfId;
-  // final double amtInvstd;
+  final String mfId;
+  final String folioId;
+  final double amtInvstd;
   final String name;
-  // final double units;
-  // final double nav;
-  // final double curValue;
+  final double units;
+  final double nav;
+  final double curValue;
 
   MFData({
-    //  @required this.mfId,
+    @required this.mfId,
+    @required this.folioId,
     @required this.name,
-    // @required this.amtInvstd,
-    // @required this.units,
-    // @required this.nav,
-    // @required this.curValue,
+    @required this.amtInvstd,
+    @required this.units,
+    @required this.nav,
+    @required this.curValue,
   });
 
   static fromSnapshot(DocumentSnapshot document) {
     return MFData(
-      //id: document['id'],
-      name: document['name'],
-    );
+        //id: document['id'],
+        name: document['name'],
+        folioId: document['folioId'],
+        mfId: document['mdId'],
+        amtInvstd: document['amtInvstd'],
+        units: document['units'],
+        nav: document['nav'],
+        curValue: document['curValue']);
   }
 }
--- lib/ui/mf/addMf.dart
+++ lib/ui/mf/addMf.dart
@@ -7,6 +7,7 @@ import 'package:sink/models/mfData.dart';
 import 'package:sink/redux/actions.dart';
 import 'package:sink/redux/selectors.dart';
 import 'package:sink/redux/state.dart';
+import 'package:sink/ui/common/number_input.dart';
 import 'package:sink/ui/common/text_input.dart';
 import 'package:sink/ui/forms/color_grid.dart';
 import 'package:uuid/uuid.dart';
@@ -25,8 +26,8 @@ class AddMfForm extends StatefulWidget {
 }
 
 class AddFmFormState extends State<AddMfForm> {
-  String mfName;
-
+  String mfName, mfId, folioId;
+  double amtInvstd, units, nav, curValue;
   void handleNameChange(String newName) {
     setState(() {
       this.mfName = newName;
@@ -50,37 +51,120 @@ class AddFmFormState extends State<AddMfForm> {
       converter: (state) => _ViewModel.fromState(state),
       builder: (BuildContext context, _ViewModel vm) {
         return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).backgroundColor,
-            title: Text('New Mutual Fund'),
-            actions: <Widget>[
-              IconButton(
-                disabledColor: Colors.grey,
-                iconSize: 28.0,
-                icon: Icon(Icons.check),
-                onPressed: !isBlank(mfName)
-                    ? () => handleSave(vm.onSave, context)
-                    : null,
-              ),
-            ],
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: Column(
-                children: <Widget>[
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).backgroundColor,
+              title: Text('New Mutual Fund'),
+              actions: <Widget>[
+                IconButton(
+                  disabledColor: Colors.grey,
+                  iconSize: 28.0,
+                  icon: Icon(Icons.check),
+                  onPressed: !isBlank(mfName)
+                      ? () => handleSave(vm.onSave, context)
+                      : null,
+                ),
+              ],
+            ),
+            body: Scrollbar(
+                child: ListView(
+                    physics: BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    padding: EdgeInsets.all(8.0),
+                    children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ClearableTextInput(
-                      hintText: "Mf Name",
-                      onChange: (value) => handleNameChange(value),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClearableTextInput(
+                              hintText: "Mf Name",
+                              onChange: (value) => handleNameChange(value),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClearableTextInput(
+                              hintText: "Mf Id",
+                              onChange: (value) =>
+                                  {setState(() => this.mfId = value)},
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClearableTextInput(
+                              hintText: "Folio Number",
+                              onChange: (value) =>
+                                  {setState(() => this.folioId = value)},
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClearableNumberInput(
+                              onChange: (value) {
+                                setState(() {
+                                  this.amtInvstd = value;
+                                });
+                              },
+                              value: this.amtInvstd,
+                              hintText: 'Amount Invested',
+                              style: Theme.of(context).textTheme.body1,
+                              //contentPadding: inputPadding,
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClearableNumberInput(
+                              onChange: (value) {
+                                setState(() {
+                                  this.units = value;
+                                });
+                              },
+                              value: this.units,
+                              hintText: 'Total Units',
+                              style: Theme.of(context).textTheme.body1,
+                              //contentPadding: inputPadding,
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClearableNumberInput(
+                              onChange: (value) {
+                                setState(() {
+                                  this.nav = value;
+                                  this.curValue = this.amtInvstd * this.nav;
+                                });
+                              },
+                              value: this.nav,
+                              hintText: 'Current Nav',
+                              style: Theme.of(context).textTheme.body1,
+                              //contentPadding: inputPadding,
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClearableNumberInput(
+                              onChange: (value) {
+                                setState(() {
+                                  this.curValue = value;
+                                });
+                              },
+                              value: this.curValue,
+                              hintText: 'Current Valuation',
+                              style: Theme.of(context).textTheme.body1,
+                              //contentPadding: inputPadding,
+                              border: OutlineInputBorder(),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        );
+                ])));
       },
     );
   }
